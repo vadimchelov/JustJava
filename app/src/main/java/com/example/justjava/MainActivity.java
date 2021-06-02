40a1f779e46f95981451e9e0fb9d3246f1ecae44
vadimchelov <vadimchelov@users.noreply.github.com>
2021-06-02 18:04:55 +0600

    fourth commit
    - interaction with Gmail
    ConstrainLayout changed to LinearLayout
    - added styles and localization

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button orderButton = findViewById(R.id.order_button);
+        Button orderButton = findViewById(R.id.send_an_order_button);
         Button incrementPriceButton = findViewById(R.id.increment_price_button);
         Button decrementPriceButton = findViewById(R.id.decrement_price_button);
 
@@ -35,81 +35,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.order_button) {
-            displayOrder(numberOfCoffees * 3);
-            Toast toast = Toast.makeText(this, "Order is accepted!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
+
         if (v.getId() == R.id.increment_price_button) {
-            if (numberOfCoffees < 99) {
+            if (numberOfCoffees < 9) {
                 numberOfCoffees++;
-                display(Integer.toString(numberOfCoffees));
+                displayQuantityOfCoffees(Integer.toString(numberOfCoffees));
             }
         }
 
         if (v.getId() == R.id.decrement_price_button) {
             if (numberOfCoffees > 0) {
                 numberOfCoffees--;
-                display(Integer.toString(numberOfCoffees));
+                displayQuantityOfCoffees(Integer.toString(numberOfCoffees));
             }
         }
 
-//        switch (v.getId()) {
-//            case R.id.order_button:
-//                displayOrder(numberOfCoffees * 5);
-//                Toast toast = Toast.makeText(this, "Order is accepted!",Toast.LENGTH_SHORT);
-//                toast.show();
-//                break;
-//            case R.id.increment_price_button:
-//                if (numberOfCoffees < 99) {
-//                    numberOfCoffees++;
-//                    display(Integer.toString(numberOfCoffees));
-//                }
-//                break;
-//            case R.id.decrement_price_button:
-//                if (numberOfCoffees > 0) {
-//                    numberOfCoffees--;
-//                    display(Integer.toString(numberOfCoffees));
-//                }
-//                break;
-//        }
+        if (v.getId() == R.id.send_an_order_button) {
+            sendAnOrder();
+            Toast toast = Toast.makeText(this, "Order is accepted!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
-    private void display(String number) {
-        TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(number);
+    private void displayQuantityOfCoffees(String number) {
+        TextView quantity = findViewById(R.id.quantity_of_coffees_text_view);
+        quantity.setText(number);
     }
 
-
-    private void displayOrder(int number) {
-        TextView orderTextView = findViewById(R.id.order_text_view);
-        CheckBox hasWhippedCream = findViewById(R.id.whipped_cream_topping_check_box);
-        CheckBox hasChocolate = findViewById(R.id.chocolate_topping_check_box);
+    private void sendAnOrder() {
+        String orderList = createOrderList(numberOfCoffees * 3);
         EditText customerEditText = findViewById(R.id.customer_name_edit_text);
 
+        String uriText =
+                "mailto:example@gmail.com" +
+                        "?subject=" + Uri.encode(customerEditText.getText().toString()) +
+                        "&body=" + Uri.encode(orderList);
+
+        Uri uri = Uri.parse(uriText);
+
+        Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+        sendIntent.setData(uri);
+        startActivity(Intent.createChooser(sendIntent, "Send email"));
+    }
+
+    private String createOrderList(int number) {
+        CheckBox hasWhippedCream = findViewById(R.id.topping_whipped_cream_check_box);
+        CheckBox hasChocolate = findViewById(R.id.topping_chocolate_check_box);
+
         /* надбавка стоимости доппингов к цене */
-        String orderList = "Name: " + customerEditText.getText() + "\nQuantity: " + numberOfCoffees;
+        String orderList = "Quantity: " + numberOfCoffees + " coffees";
         if (hasWhippedCream.isChecked()) {
             number += numberOfCoffees;
-            orderList += "\n\t+ whipped cream";
+            orderList += "\n\t+ add whipped cream";
         }
+
         if (hasChocolate.isChecked()) {
             number += 2 * numberOfCoffees;
-            orderList += "\n\t+ some chocolate";
+            orderList += "\n\t+ add some chocolate";
         }
+
         String orderPrice = NumberFormat.getCurrencyInstance(Locale.US).format(number);
-        orderList += "\nTotal: " + orderPrice;
+        orderList += "\n\nTotal: " + orderPrice;
 
-        orderTextView.setText(orderList);
-
-
-        /*неудачная попытка в Intent*/
-//
-//        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + "vadimchelov@gmail.com"));
-//        intent.putExtra(Intent.EXTRA_SUBJECT, customerEditText.getText());
-//        intent.putExtra(Intent.EXTRA_TEXT, orderList);
-//        if (intent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(Intent.createChooser(intent, getString(R.string.сhoose_email_app)));
-//        }
+        return orderList;
     }
 }
